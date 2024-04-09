1658a471d7a7233a07f7dc18cd90ae5e7f45b408
Rohit.N <140035253+RohitN002@users.noreply.github.com>
2024-04-09 10:34:03 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chatapp-6vpi.onrender.com/", {
+			const socket = io("https://chatapp-qwx5.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
